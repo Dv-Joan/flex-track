9ce50009dd327ee26e86bfe2f9630ad04a267fac
dv-joan <joan300501@gmail.com>
2023-05-30 17:15:07 -0500

    feat: Shadcn card manipulating

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,8 +1,60 @@
-import React from 'react'
+import * as React from "react"
 
+import { Button } from "@/components/ui/button"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
 function page() {
     return (
         <section className="text-gray-400">
+            <Card className="w-[350px]">
+                <CardHeader>
+                    <CardTitle>Create project</CardTitle>
+                    <CardDescription>Deploy your new project in one-click.</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <form>
+                        <div className="grid items-center w-full gap-4">
+                            <div className="flex flex-col space-y-1.5">
+                                <Label htmlFor="name">Name</Label>
+                                <Input id="name" placeholder="Name of your project" />
+                            </div>
+                            <div className="flex flex-col space-y-1.5">
+                                <Label htmlFor="name">Framework</Label>
+                                <Select>
+                                    <SelectTrigger>
+                                        <SelectValue placeholder="Select" />
+                                        <SelectContent position="popper">
+                                            <SelectItem value="next">Next.js</SelectItem>
+                                            <SelectItem value="sveltekit">SvelteKit</SelectItem>
+                                            <SelectItem value="astro">Astro</SelectItem>
+                                            <SelectItem value="nuxt">Nuxt.js</SelectItem>
+                                        </SelectContent>
+                                    </SelectTrigger>
+                                </Select>
+                            </div>
+                        </div>
+                    </form>
+                </CardContent>
+                <CardFooter className="flex justify-between">
+                    <Button variant="ghost">Cancel</Button>
+                    <Button>Deploy</Button>
+                </CardFooter>
+            </Card>
             <div className="container px-5 py-24 mx-auto">
                 <div className="flex flex-wrap -m-4">
                     <div className="w-full p-4 lg:w-1/4 md:w-1/2">
--- app/services/page.tsx
+++ app/services/page.tsx
@@ -6,15 +6,15 @@ function page() {
             <div className="container px-5 py-24 mx-auto">
                 <div className="flex flex-col">
                     <div className="h-1 overflow-hidden bg-gray-800 rounded">
-                        <div className="w-24 h-full bg-indigo-500"></div>
+                        <div className="w-24 h-full bg-white"></div>
                     </div>
                     <div className="flex flex-col flex-wrap py-6 mb-12 sm:flex-row">
                         <h1 className="mb-2 text-2xl font-medium text-white sm:w-2/5 title-font sm:mb-0">Space The Final Frontier</h1>
                         <p className="pl-0 text-base leading-relaxed sm:w-3/5 sm:pl-10">Street art subway tile salvia four dollar toast bitters selfies quinoa yuccie synth meditation iPhone intelligentsia prism tofu. Viral gochujang bitters dreamcatcher.</p>
                     </div>
                 </div>
-                <div className="flex flex-wrap -mx-4 -mt-4 -mb-10 sm:-m-4">
-                    <div className="p-4 mb-6 md:w-1/3 sm:mb-0">
+                <div className="flex flex-wrap -mx-10 -mt-4 -mb-10 sm:-m-4">
+                    <div className="p-4 mb-6 md:w-1/4 sm:mb-0">
                         <div className="h-64 overflow-hidden rounded-lg">
                             <img alt="content" className="object-cover object-center w-full h-full" src="https://dummyimage.com/1203x503" />
                         </div>
